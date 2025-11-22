991c82eaecf7bff55defc4c60fe28447f959a3cb
viniciusmarketing9229-bit <viniciusmarketing9229@gmail.com>
2025-11-22 02:42:36 -0300

    Delete generate.js

--- generate.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req,res){
- const {product}=req.body;
- res.status(200).json({
-   output:`Aqui está um anúncio gerado automaticamente para o produto: ${product}`
- });
-}
